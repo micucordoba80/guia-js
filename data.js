c119dcf95a81be21c21cdcf5abd7aa86721edcc5
mica cordoba <micu.cordoba80@gmail.com>
2026-05-02 15:40:49 -0300

    cambio del 18-20 de main a data

--- data.js
+++ data.js
@@ -1,1 +1,31 @@
-data.js
+//ejercicio 18: Array de objetos
+
+const personajes = [
+ { nombre: "Luna", vida: 100, tipo: "maga", nivel: 3 },
+ { nombre: "Kai", vida: 120, tipo: "guerrero", nivel: 4 },
+ { nombre: "Nina", vida: 90, tipo: "arquera", nivel: 2 }
+];
+
+for (let i = 0; i < personajes.length; i++) {
+    console.log(personajes[i].nombre + " es " + personajes[i].tipo + " y está en nivel " + personajes[i].nivel);
+}
+
+// ejercicio 19: Filtrar personajes por nivel
+
+const personajesFuertes = personajes.filter((personaje) => {
+    return personaje.nivel >= 3;
+});
+
+console.log("Personajes con nivel 3 o superior:");
+personajesFuertes.forEach((personaje) => {
+    console.log(personaje.nombre + " - Nivel: " + personaje.nivel);
+});
+
+
+// ejercicio 20: Obtener solo los nombres
+
+const nombres = personajes.map((personaje) => {
+    return personaje.nombre;
+});
+
+console.log(nombres);
--- main.js
+++ main.js
@@ -227,38 +227,6 @@ subirNivel(jugador2);
 ganarMonedas(jugador2, 50);
 
 
-//ejercicio 18: Array de objetos
-
-const personajes = [
- { nombre: "Luna", vida: 100, tipo: "maga", nivel: 3 },
- { nombre: "Kai", vida: 120, tipo: "guerrero", nivel: 4 },
- { nombre: "Nina", vida: 90, tipo: "arquera", nivel: 2 }
-];
-
-for (let i = 0; i < personajes.length; i++) {
-    console.log(personajes[i].nombre + " es " + personajes[i].tipo + " y está en nivel " + personajes[i].nivel);
-}
-
-// ejercicio 19: Filtrar personajes por nivel
-
-const personajesFuertes = personajes.filter((personaje) => {
-    return personaje.nivel >= 3;
-});
-
-console.log("Personajes con nivel 3 o superior:");
-personajesFuertes.forEach((personaje) => {
-    console.log(personaje.nombre + " - Nivel: " + personaje.nivel);
-});
-
-
-// ejercicio 20: Obtener solo los nombres
-
-const nombres = personajes.map((personaje) => {
-    return personaje.nombre;
-});
-
-console.log(nombres);
-
 // ejercicio 21: Buscar un personaje
 
 const personajeBuscado = personajes.find((personaje) => {
@@ -270,6 +238,7 @@ if (personajeBuscado) {
 } else {
     console.log("Personaje no encontrado");
 }   
+
 
 // ejercicio 22: Calcular total de vida
 
